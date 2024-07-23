e7860c5ab5b3662dbc00a4586b710762c35c0721
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2024-07-23 14:25:57 -0400

    Time: 56 ms (18.38%), Space: 17.9 MB (46.55%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -10,16 +10,31 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        # if not root:
+        #     return None
+        # visited,queue=set(),deque([root]) 
+        # while queue:
+        #     right_node=None
+        #     for i in range(len(queue)):
+        #         cur_node=queue.popleft()
+        #         cur_node.next,right_node=right_node,cur_node
+        #         if cur_node.right:
+        #             queue.extend([cur_node.right,cur_node.left])
+        # return root
         if not root:
             return None
-        visited,queue=set(),deque([root]) 
+        queue=deque([root])
         while queue:
-            right_node=None
-            for i in range(len(queue)):
+            prev_node=None
+            for _ in range(len(queue)):
                 cur_node=queue.popleft()
-                cur_node.next,right_node=right_node,cur_node
+                if prev_node:
+                    prev_node.next=cur_node
+                if cur_node.left:
+                    queue.append(cur_node.left)
                 if cur_node.right:
-                    queue.extend([cur_node.right,cur_node.left])
-        return root
+                    queue.append(cur_node.right)
 
+                prev_node=cur_node
+        return root
         
