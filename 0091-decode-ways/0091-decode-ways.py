acc4106080e3f97350e0e7ac73d140364df5ff8e
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2024-10-02 02:40:30 -0400

    Time: 38 ms (43.65%), Space: 16.6 MB (54.89%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -2,9 +2,9 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         if not s or s[0] == '0':
             return 0
-        num,n=int(s),len(s)
-        count=set()
-        l,r=0,len(s)-1
+        n=len(s)
+        #count=set()
+        #l,r=0,len(s)-1
 #         hm={
 #             "A":1,"B":2,"C":3,"D":4,"E":5,"F":6
 # ,"G":7,"H":8 ,"I":9 ,"J":10,"K":11,"L":12,"M":13,"N":14,"O":15,"P":16,"Q":17,"R":18,"S":19,"T":20,"U":21,"V":22,"W":23,"X":24,"Y":25,"Z":26     }
