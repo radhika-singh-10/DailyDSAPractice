04dfbba52519be2f00eeb6916dd2e2e2673ded09
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2024-11-20 11:00:55 -0500

    Time: 0 ms (100%), Space: 16.6 MB (23.04%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,13 +1,26 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        #base case, recurrence formula ,recurrence 
         if n<=2:
             return n
+        elif n<=0:
+            return 0
         else:
             dp=[0]*(n+1)
-            dp[0],dp[1],dp[2]=0,1,2
-            i=3
+            dp[0],dp[1]=1,1
             res=0
-            while i<=n:
+            for i in range(2,n+1): 
                 dp[i]=dp[i-1]+dp[i-2]
-                i=i+1
             return dp[n]
+
+            #time limit exceeded
+            # def recursion(n):
+            #     if n<=0:
+            #         return 0
+            #     elif 1<=n<=2:
+            #         return n
+            #     else:
+
+            #         return recursion(n-1)+recursion(n-2)
+
+            # return recursion(n)
