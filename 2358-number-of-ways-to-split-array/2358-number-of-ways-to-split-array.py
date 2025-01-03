bf4e0e92f9527c392a84266f839397dacaa08d66
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-01-03 13:43:58 -0500

    Time: 630 ms (5.01%), Space: 32.7 MB (37.53%) - LeetHub

---
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.py
@@ -0,0 +1,32 @@
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        #edge cases-> 1 element -> 1 split
+        #0 ->1 split
+        #all unqiue values or any duplicate values?
+        #integers only?? or fractions?
+        #positive numbers, negative nums, zeroes?
+        #can it be sorted or always unsorted? in case of sorted in increasing order->0 for decreasing -> n-1 where n-length of list
+
+        #approach
+        #prefix sum can be used here
+        #lsum>=rsum; lsum+arr[i]>=rsum
+        #calculate the rsum 
+        #then lsum+arr[i]>=rsum-arr[i] split+=1
+        #else continue
+        #tc->0(n) #sc-0(1)
+        lsum,rsum=0,0
+        n,split=len(nums),0
+        for i in range(n):
+            rsum+=nums[i]
+
+        for i in range(n-1):
+            print(i,lsum+nums[i],rsum-nums[i],lsum+nums[i]>=rsum-nums[i])
+            lsum+=nums[i]
+            rsum-=nums[i]
+            if lsum>=rsum:
+                split+=1
+                
+            else:
+                continue
+
+        return split
