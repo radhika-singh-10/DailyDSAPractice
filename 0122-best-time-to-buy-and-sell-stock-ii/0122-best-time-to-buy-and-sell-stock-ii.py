f88a7789a7ead1f0b6e62138bd5d16c07a7e8297
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-01-13 16:18:06 -0500

    Time: 0 ms (100%), Space: 18.8 MB (36.29%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,12 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        i=0
+        i=1
         profit=0
-        # while i<len(prices):
-        #     if prices[i]>prices[i-1] : 
-        #         profit+=prices[i]-prices[i-1]
-        #         print(profit,prices[i]-prices[i-1])
-        #     i=i+1
+        while i<len(prices):
+            if prices[i]>prices[i-1] : 
+                profit+=prices[i]-prices[i-1]
+                print(profit,prices[i]-prices[i-1])
+            i=i+1
         # n,valley,peak=len(prices),prices[0],prices[0]
         # while i<n-1:
         #     while i<n-1 and prices[i]<=prices[i+1]:
@@ -16,18 +16,18 @@ class Solution:
         #         i+=1
         #     valley=prices[i]
         #     profit+=peak-valley
-        # return profit
-        i = 0
-        valley = prices[0]
-        peak = prices[0]
-        maxprofit = 0
-        while i < len(prices) - 1:
-            while i < len(prices) - 1 and prices[i] >= prices[i + 1]:
-                i += 1
-            valley = prices[i]
-            while i < len(prices) - 1 and prices[i] <= prices[i + 1]:
-                i += 1
-            peak = prices[i]
-            maxprofit += peak - valley
-        return maxprofit
+        return profit
+        # i = 0
+        # valley = prices[0]
+        # peak = prices[0]
+        # maxprofit = 0
+        # while i < len(prices) - 1:
+        #     while i < len(prices) - 1 and prices[i] >= prices[i + 1]:
+        #         i += 1
+        #     valley = prices[i]
+        #     while i < len(prices) - 1 and prices[i] <= prices[i + 1]:
+        #         i += 1
+        #     peak = prices[i]
+        #     maxprofit += peak - valley
+        # return maxprofit
 
