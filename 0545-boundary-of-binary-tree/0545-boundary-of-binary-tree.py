a33f7008252e2c64118fa9433de3e226ff991e4d
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-01-26 16:07:16 -0500

    Time: 7 ms (7.02%), Space: 19.3 MB (16.88%) - LeetHub

--- 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.py
+++ 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.py
@@ -6,4 +6,56 @@
 #         self.right = right
 class Solution:
     def boundaryOfBinaryTree(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        res=[]
+        #root - append in the res
+        #iterative traverse left and check if the node has leaf which is not empty , append
+        #traverse to leaf - dfs
+        #iterative traverse right and check if the node has leaf which is not empty
         
+        if not root:
+            return []
+        
+        def isLeafNode(node):
+            return not node.left and not node.right
+        if root and isLeafNode(root):
+            return [root.val]
+        def getLeftBoundary(node):
+            boundary=[]
+            while node:
+                if not isLeafNode(node):
+                    boundary.append(node.val)
+                if node.left:
+                    node=node.left
+                else:
+                    node=node.right
+                    
+            return boundary
+        
+        def getRightBoundary(node):
+            boundary=[]
+            while node:
+                if not isLeafNode(node):
+                    boundary.append(node.val)
+                if node.right:
+                    node=node.right
+                else:
+                    node=node.left
+                    
+            return boundary[::-1]
+    
+        def getLeaves(node):
+            if not node:
+                return []
+            if isLeafNode(node):
+                return [node.val]
+            return getLeaves(node.left) + getLeaves(node.right)
+        
+        left_boundary = getLeftBoundary(root.left) if root.left else []
+        right_boundary = getRightBoundary(root.right) if root else []
+        leaves = getLeaves(root) if root else []
+        return [root.val] + left_boundary + leaves + right_boundary
+
+            
+            
