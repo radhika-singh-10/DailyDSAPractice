651acadd50a74e7b829eda63fbcb32705a4cf445
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-02-06 17:54:12 -0500

    Time: 195 ms (50.58%), Space: 33.4 MB (82.85%) - LeetHub

--- 2869-longest-non-decreasing-subarray-from-two-arrays/2869-longest-non-decreasing-subarray-from-two-arrays.py
+++ 2869-longest-non-decreasing-subarray-from-two-arrays/2869-longest-non-decreasing-subarray-from-two-arrays.py
@@ -5,7 +5,7 @@ class Solution:
         res = 1
         dp1, dp2 = 1, 1  
         for i in range(1, n):
-            new_dp1 = new_dp2 = 1
+            new_dp1 , new_dp2 = 1, 1
             if nums1[i] >= nums1[i - 1]:
                 new_dp1 = max(new_dp1, dp1 + 1)
             if nums1[i] >= nums2[i - 1]:
