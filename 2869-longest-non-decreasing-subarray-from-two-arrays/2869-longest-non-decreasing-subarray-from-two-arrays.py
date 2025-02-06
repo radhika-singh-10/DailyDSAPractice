2fd9f5ed80e4c34a1a8e2037a63d406060c33d21
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-02-06 17:50:50 -0500

    Time: 187 ms (57.85%), Space: 33.5 MB (82.85%) - LeetHub

--- 2869-longest-non-decreasing-subarray-from-two-arrays/2869-longest-non-decreasing-subarray-from-two-arrays.py
+++ 2869-longest-non-decreasing-subarray-from-two-arrays/2869-longest-non-decreasing-subarray-from-two-arrays.py
@@ -2,8 +2,8 @@ class Solution:
     def maxNonDecreasingLength(self, nums1: List[int], nums2: List[int]) -> int:
         #non-decreasing-increasing or equal
         n = len(nums1)
-        res = 1 
-        dp1, dp2 = 1, 1 
+        res = 1
+        dp1, dp2 = 1, 1  
         for i in range(1, n):
             new_dp1 = new_dp2 = 1
             if nums1[i] >= nums1[i - 1]:
