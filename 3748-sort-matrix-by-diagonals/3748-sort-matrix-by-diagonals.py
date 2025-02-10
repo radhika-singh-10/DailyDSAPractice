670ea85b54798ab5f1e9e24017efb3210de95964
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-02-09 19:27:31 -0500

    Time: 10 ms (100%), Space: 17.8 MB (100%) - LeetHub

--- 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.py
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.py
@@ -6,13 +6,17 @@ class Solution:
             for j in range(n):
                 if i-j>=0:
                     heappush(hm[i-j],-grid[i][j])
+                    #print(hm[i-j])
                 else:
                     heappush(hm[i-j],grid[i][j])
+            
 
         for i in range(n):
             for j in range(n):
                 if i-j>=0:
+                    #print(hm[i-j])
                     grid[i][j]=-heappop(hm[i-j])
+                    #print(grid[i][j])
                 else:
                     grid[i][j]=heappop(hm[i-j])
         return grid
