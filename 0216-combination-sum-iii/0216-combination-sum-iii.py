ffa2659e0be59c36c79925688bb9033b7686b713
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-02-16 03:20:03 -0500

    Time: 0 ms (100%), Space: 17.6 MB (86.49%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,38 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        nums=[i for i in range(1,10)]
+        res=set()
+        #
+        #backtracking
+        #check if nums[i]+total==k: append in res and return 
+        #if i==len(nums) or total>k:return
+        #temp.append(nums[i]) 
+        #total+=nums[i]
+        #backtrack_subset(i,total+nums[i],temp)
+        #temp.pop()
+        #backtrack_subset(i,total,temp)
+
+        def backtrack_subset(i,total,temp):
+            if len(temp) == k:
+                if total == n:
+                    res.add(tuple(temp[:]))
+                return
+
+            for i in range(i,10):
+                if total+i>n:
+                    return 
+
+                temp.append(i) 
+                backtrack_subset(i+1,total+i,temp)
+                temp.pop()
+                
+            
+            # temp.append(nums[i]) 
+            # #total+=nums[i]
+            # backtrack_subset(i+1,total+nums[i],temp)
+            # temp.pop()
+            # backtrack_subset(i+1,total,temp)
+
+        backtrack_subset(1,0,[])
+
+        return [combination for combination in res]
