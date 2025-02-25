0332768dfb341eaa0de0af6e6f6d39fc156b9df2
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-02-25 12:23:57 -0500

    Time: 26 ms (50.57%), Space: 29.3 MB (56.42%) - LeetHub

--- 1512-design-underground-system/1512-design-underground-system.py
+++ 1512-design-underground-system/1512-design-underground-system.py
@@ -4,14 +4,19 @@ class UndergroundSystem:
         #station dict customer id - [name, time]
         self.station=collections.defaultdict(lambda : [0, 0])
 
+        #name,id = remove by id
+        #checkin - startstation ,t (append)
+        #checkout - endStation ,
+
     def checkIn(self, id: int, stationName: str, t: int) -> None:
         self.station[id]=[stationName,t]
-
-
+        
     def checkOut(self, id: int, stationName: str, t: int) -> None:
-        name,time=self.station.pop(id)
-        self.station[(name,stationName)][0]+=(t-time)
-        self.station[(name,stationName)][1]+=1
+        startStation,time=self.station.pop(id)
+        self.station[(startStation,stationName)][0]+=(t-time)
+        self.station[(startStation,stationName)][1]+=1
+        # print(self.station)
+        # print(self.station[(startStation,stationName)])
 
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
