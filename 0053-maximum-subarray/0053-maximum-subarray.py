c760bc89ef9a802722cf7d24acb9680ad6c9450f
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-03-03 13:23:24 -0500

    Time: 66 ms (52.1%), Space: 31.9 MB (88.85%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,9 +4,9 @@ class Solution:
         #kadane's algorithm 
 
         #l,r-2ptr approach
-        #while r<len(nums) and cur_sum +nums[r]>=cur_sum: r=r+1
-        #if cur_sum+nums[r]<cur_sum: r=r+1, l=r
+        #cur_sum+=nums[r]
         #res=max(res,cur_sum)
+        #while r<len(nums) and cur_sum <0: cur_sum-=nums[l] l=l+1
         #tc-0(n), sc-0(1)
 
         n,l,res,cur_sum=len(nums),0,-float('inf'),0
