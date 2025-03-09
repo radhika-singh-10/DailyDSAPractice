9fd632114ce7fc88994dc51466f4cca17b71f8c1
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-03-09 14:40:23 -0400

    Time: 47 ms (47.14%), Space: 18.1 MB (57.17%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -3,6 +3,26 @@ class Solution:
         n,m,o=len(s1),len(s2),len(s3)
         if n+m!=o:
             return False
+        # dp={}
+        # def dfs(i,j,k):
+        #     if k==o and i==n and j==m:
+        #         return True
+        #     if (i,j) in dp:
+        #         return dp[(i,j)]
+        #     res=False
+        #     if i<n and s1[i]==s3[k]:
+        #         res=dfs(i+1,j,k+1)
+        #     if j<m and s2[j]==s3[k]:
+        #         res=dfs(i,j+1,k+1)
+            
+        #     dp[(i,j)]=res
+        #     return res
+        # return dfs(0,0,0)
+
+            
+        # n,m,o=len(s1),len(s2),len(s3)
+        # if n+m!=o:
+        #     return False
         dp=[[False]*(m+1) for _ in range(n+1)]
         dp[n][m]=True
         for i in range(n,-1,-1):
