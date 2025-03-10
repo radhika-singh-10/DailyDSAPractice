1ba4bf5cd9c393b84ef73803e8b62755148c1508
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-03-10 13:02:56 -0400

    Time: 4 ms (6.43%), Space: 17.7 MB (76.28%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.py
+++ 0289-game-of-life/0289-game-of-life.py
@@ -4,17 +4,17 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         neighbors = [(1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1), (1,1)]
-        rows,cols = len(board),len(board[0])
-        copy_board = [[board[row][col] for col in range(cols)] for row in range(rows)]
-        for row in range(rows):
-            for col in range(cols):
+        n,m = len(board),len(board[0])
+        deep_copy_board = [[board[r][c] for c in range(m)] for r in range(n)]
+        for row in range(n):
+            for col in range(m):
                 live = 0
                 for neighbor in neighbors:
                     r = (row + neighbor[0])
                     c = (col + neighbor[1])
-                    if (r < rows and r >= 0) and (c < cols and c >= 0) and (copy_board[r][c] == 1):
+                    if (0<=r<n) and (0<=c<m) and (deep_copy_board[r][c] == 1):
                         live+=1
-                if copy_board[row][col] == 1 and (live < 2 or live > 3):
+                if deep_copy_board[row][col] == 1 and (live < 2 or live > 3):
                     board[row][col] = 0
-                if copy_board[row][col] == 0 and live == 3:
+                if deep_copy_board[row][col] == 0 and live == 3:
                     board[row][col] = 1
