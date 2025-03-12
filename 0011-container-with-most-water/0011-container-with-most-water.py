55d009efc88452aa44bbdfce90f1bf84d385b224
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-03-12 15:13:59 -0400

    Time: 107 ms (38.46%), Space: 28.5 MB (67.42%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,9 +1,12 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+
+
+        
         #edge cases->height==1->height[0]
         #any 0-> l=l+1
         #only positive number? no negative or 0 or decimal
-
+        #what if - all the 
         if len(height)==1:
             return height[0]
         res,l,r,max_area=0,0,len(height)-1,0
@@ -11,7 +14,11 @@ class Solution:
             max_area=max(max_area,min(height[l],height[r])*abs(r-l))
             if height[l]<height[r]:
                 l=l+1
+            #elif height[r]>=height[l]:
             else:
                 r=r-1
+            # else:
+            #     l+=1
+            #     r-=1
             
         return max_area
