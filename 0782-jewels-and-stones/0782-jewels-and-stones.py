cb76bcdf24d5db952605f382b1cf611fe47a7c5f
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-03-27 13:54:49 -0400

    Time: 0 ms (100%), Space: 17.6 MB (82.94%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -1,15 +1,21 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         res=0
-        stoneMap={}
-        for i in range(len(stones)):
-            if stones[i] in stoneMap:
-                stoneMap[stones[i]]+=1
-            else:
-                stoneMap[stones[i]]=1
-        for k,v in enumerate(stoneMap.items()):
-            
-            if v[0] in jewels:
-               res+=v[1]
-            
+        jewels=set([j for j in jewels])
+        for s in stones:
+            if s in jewels:
+                res+=1
+
         return res
+        # stoneMap={}
+        # for i in range(len(stones)):
+        #     if stones[i] in stoneMap:
+        #         stoneMap[stones[i]]+=1
+        #     else:
+        #         stoneMap[stones[i]]=1
+        # for k,v in enumerate(stoneMap.items()):
+            
+        #     if v[0] in jewels:
+        #        res+=v[1]
+            
+        # return res
