1ff9916961291249eed181a683b7551199d440ae
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-03-27 16:17:51 -0400

    Time: 159 ms (54.92%), Space: 19.2 MB (10.4%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,6 +1,8 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        #google question 
         l,r=1,max(piles)
+        #we take max instead of last element for r because its unsorted piles
         while l<r:
             m=(l+r)//2
             hr_spent=0
