a2c40300aeb6687f2649089343d79be7160cfb7a
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-03-26 23:41:15 -0400

    Time: 15 ms (63.7%), Space: 23 MB (42.79%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -17,8 +17,8 @@ class Solution:
             #     cur_sum=0
             # elif node.val+cur_sum>cur_sum:
             #     cur_sum+=node.val
-            left_sum=max(0,dfs(node.left))
-            right_sum=max(0,dfs(node.right))
+            
+            left_sum,right_sum=max(0,dfs(node.left)),max(0,dfs(node.right))
             res=max(res,left_sum+right_sum+node.val)
             return max(left_sum+node.val,right_sum+node.val)
             #res=max(res,node.val+dfs(node.left,cur_sum)+dfs(node.right,cur_sum))
