63c9f032b55586ef41634f9b6e5a915319a82350
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-03-28 12:04:30 -0400

    Time: 51 ms (25.36%), Space: 19.3 MB (80.27%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -0,0 +1,35 @@
+from collections import deque
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        if not heights or not heights[0]:
+            return []
+
+        n, m = len(heights), len(heights[0])
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        def bfs(starts):
+            visited = set()
+            queue = deque(starts)
+            while queue:
+                r, c = queue.popleft()
+                visited.add((r, c))
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if (0 <= nr < n and 0 <= nc < m and
+                        (nr, nc) not in visited and
+                        heights[nr][nc] >= heights[r][c]):
+                        queue.append((nr, nc))
+            return visited
+
+        pacific_starts = [(0, j) for j in range(m)] + [(i, 0) for i in range(n)]
+        atlantic_starts = [(n - 1, j) for j in range(m)] + [(i, m - 1) for i in range(n)]
+
+        pacific_reachable = bfs(pacific_starts)
+        atlantic_reachable = bfs(atlantic_starts)
+
+        return list(pacific_reachable & atlantic_reachable)
+
+
+
+
+
