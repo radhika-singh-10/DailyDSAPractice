2cab4988ea557db6733f1ca42393862a0650453c
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-03-29 13:01:56 -0400

    Time: 481 ms (91.35%), Space: 0B (100%) - LeetHub

---
+++ 1204-new-users-daily-count/1204-new-users-daily-count.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+
+
+SELECT login_date, COUNT(DISTINCT user_id) AS user_count FROM (SELECT user_id,MIN(activity_date) AS login_date FROM Traffic WHERE activity = 'login' GROUP BY user_id HAVING DATEDIFF('2019-06-30',  MIN(activity_date)) <= 90) t0 GROUP BY login_date
