36c956200ebc5ca3bb9bd3f44e7489aeb62c59b4
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-03-30 11:08:11 -0400

    Time: 15 ms (43.6%), Space: 18.1 MB (95.84%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -7,16 +7,10 @@ class Solution:
         def backtrackCheck(row,rowSet,colSet,diagonalSet,antiDiagonalSet,board):
             if row==n:
                 res.append(board)
-            for col in range(n):
-                
+            for col in range(n):   
                 isValidRowCol=(row not in rowSet) and (col not in colSet) and (row+col not in diagonalSet) and (row-col not in antiDiagonalSet)
-
                 if isValidRowCol:
                     backtrackCheck(row+1,rowSet.union({row}),colSet.union({col}),diagonalSet.union({row+col}),antiDiagonalSet.union({row-col}),board+['.'*col+'Q'+'.'*(n-col-1)])
-
-                
-
-
         backtrackCheck(0,set(),set(),set(),set(),[])
         return res
 
