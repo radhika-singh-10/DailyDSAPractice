a77d3cd038c7964b97c4e9f7e418760e501c5c8b
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-03-30 20:19:01 -0400

    Time: 0 ms (100%), Space: 17.6 MB (82.23%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
@@ -1,10 +1,12 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
         #a -> freq[ord(s[i])-ord('a')]+=1
-        freq=[0]*26
-        for i in range(len(sentence)):
-            freq[ord(sentence[i])-ord('a')]+=1
-        for i in range(26):
-            if freq[i]==0:
-                return False
-        return True
+        # freq=[0]*26
+        # for i in range(len(sentence)):
+        #     freq[ord(sentence[i])-ord('a')]+=1
+        # for i in range(26):
+        #     if freq[i]==0:
+        #         return False
+        # return True
+        seen_set=set(sentence)
+        return len(seen_set)==26
