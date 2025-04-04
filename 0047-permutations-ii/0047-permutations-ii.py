d223a674529044bcf5dce730240a734bb04bd4e2
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-04-03 20:44:26 -0400

    Time: 2 ms (96.44%), Space: 18.3 MB (9.82%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,13 +1,30 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        res=[]
-
-        self.dfs(nums,[],res)
+        res,n=[],len(nums)
+        def backtrack(index):
+            if n==index:
+                res.append(nums[:])
+                return 
+            temp=set()
+            for i in range(index,n):
+                if nums[i] not in temp:
+                    nums[i],nums[index]=nums[index],nums[i]
+                    backtrack(index+1)
+                    nums[i],nums[index]=nums[index],nums[i]
+                    temp.add(nums[i])
+            
+        backtrack(0)
         return res
 
-    def dfs(self,nums,stack,res):
-        if not nums and not res.__contains__(stack.copy()):
-            res.append(stack)
 
-        for i in range(len(nums)):
-            self.dfs(nums[:i]+nums[i+1:],stack+[nums[i]],res)
+    #     res=[]
+
+    #     self.dfs(nums,[],res)
+    #     return res
+
+    # def dfs(self,nums,stack,res):
+    #     if not nums and not res.__contains__(stack.copy()):
+    #         res.append(stack)
+
+    #     for i in range(len(nums)):
+    #         self.dfs(nums[:i]+nums[i+1:],stack+[nums[i]],res)
