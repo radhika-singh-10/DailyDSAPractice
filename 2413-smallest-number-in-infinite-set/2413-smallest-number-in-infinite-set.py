0978e1c87f38f768ad142e32f816216f3af99009
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-04-05 19:27:20 -0400

    Time: 7 ms (97.33%), Space: 18.4 MB (80.86%) - LeetHub

--- 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.py
+++ 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.py
@@ -1,25 +1,39 @@
-import heapq
+# import heapq
 class SmallestInfiniteSet:
 
     def __init__(self):
         self.heap=[]
-        for i in range(1,1001):
-            self.heap.append(i)
+        self.isPresent=set()
+        self.current=1
+        #since we haev the constraint here that is why we have added 1-1000
+        # for i in range(1,1001):
+        #     self.heap.append(i)
 
     def popSmallest(self) -> int:
         # for elem in self.heap:
         #     elem*=-1
-        heapq.heapify(self.heap)
-        # print(self.heap)
-        elem=self.heap[0]
-        heapq.heappop(self.heap)
-        return elem
+        if len(self.heap)>0:
+            res=heapq.heappop(self.heap)
+            self.isPresent.remove(res)
+        else:
+            res=self.current
+            self.current+=1
+        return res
+        # heapq.heapify(self.heap)
+        # # print(self.heap)
+        # elem=self.heap[0]
+        # heapq.heappop(self.heap)
+        # return elem
 
 
     def addBack(self, num: int) -> None:
-        if num not in self.heap:
-            self.heap.append(num)
-        heapq.heapify(self.heap)
+        if self.current<=num or num in self.isPresent:
+            return
+        heapq.heappush(self.heap,num)
+        self.isPresent.add(num)
+        # if num not in self.heap:
+        #     self.heap.append(num)
+        # heapq.heapify(self.heap)
         
 
 
