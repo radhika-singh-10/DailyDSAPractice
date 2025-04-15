c7c93b56b55330b5068bd91d928d1cdebd88ec94
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-04-15 10:54:03 -0400

    Time: 2635 ms (87.54%), Space: 20.2 MB (82.19%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -8,13 +8,23 @@ class Solution:
         #question is similar to matrix multipliction 
         dp=[[0]*(n) for _ in range(n)]
         #get(i-1,nums)*nums[i]*get(i+1,nums)
-        for l in range(n-2,0,-1):
-            for r in range(l,n-1):
-                for i in range(l,r+1):
-                    gain=nums[l-1]*nums[i]*nums[r+1]
-                    remaining=dp[l][i-1]+dp[i+1][r]
-                    dp[l][r]=max(remaining+gain,dp[l][r])
-        return dp[1][n-2]
+        # for l in range(n-2,0,-1):
+        #     for r in range(l,n-1):
+        #         for i in range(l,r+1):
+        #             gain=nums[l-1]*nums[i]*nums[r+1]
+        #             remaining=dp[l][i-1]+dp[i+1][r]
+        #             dp[l][r]=max(remaining+gain,dp[l][r])
+        # return dp[1][n-2]
+        for k in range(2,n):
+            for l in range(n-k):
+                r=l+k
+                for i in range(l+1,r):
+                    dp[l][r]=max(dp[l][r],nums[l]*nums[i]*nums[r]+dp[l][i]+dp[i][r])
+        return dp[0][n-1]
+
+
+
+        
 
 
         
