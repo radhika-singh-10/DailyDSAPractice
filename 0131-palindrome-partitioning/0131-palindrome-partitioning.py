0cd257e552a01171d146a7cf8fbfb607ef8b4fce
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-04-15 12:28:25 -0400

    Time: 51 ms (56.14%), Space: 32.8 MB (85.25%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,17 +1,27 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         #we need to iterate i by keeping las 0 and r as n-1 always
-        res=[]
-        self.backtrack(s,[],res)
+        res,n=[],len(s)
+        dp=[[False]*n for _ in range(n)]
+        self.backtrack(s,[],0,res,dp)
+        # self.backtrack(s,[],res)
         return res
-
-    def backtrack(self,s,palindrome,res):
-        if not s:
-            res.append(palindrome)
-            return 
-        for i in range(1,len(s)+1):
-            print(i,s[:i],s[:i][::-1])
-            if s[:i]==s[:i][::-1]:
-                self.backtrack(s[i:],palindrome+[s[:i]],res)
+    def backtrack(self,s,palindrome,i,res,dp):
+        if i>=len(s):
+            res.append(list(palindrome))
+        for j in range(i,len(s)):
+            if s[i]==s[j] and (j-i<=2 or dp[i+1][j-1]):
+                dp[i][j]=True
+                palindrome.append(s[i:j+1])
+                self.backtrack(s,palindrome,j+1,res,dp)
+                palindrome.pop()
+    # def backtrack(self,s,palindrome,res):
+    #     if not s:
+    #         res.append(palindrome)
+    #         return 
+    #     for i in range(1,len(s)+1):
+    #         print(i,s[:i],s[:i][::-1],s,palindrome,s[i:])
+    #         if s[:i]==s[:i][::-1]:
+    #             self.backtrack(s[i:],palindrome+[s[:i]],res)
     
             
