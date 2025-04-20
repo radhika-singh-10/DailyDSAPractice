3ac13da366ae991e5956ef297cb98f6778710830
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-04-19 20:05:32 -0400

    Time: 218 ms (27.26%), Space: 22.3 MB (18.91%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.py
@@ -2,15 +2,21 @@ class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
         #similar to koko eating bananas
         #similar to questions asked in mock interview of youtube channel, (keerti purswani)
-        #we need to find a balance point
+        #we need to find a balance point as the ships max capacity cant be smaller than max_weight so we check in between max_weight and total_weight
+        #
+
+
         max_weight,total_weight,res=-1,0,0
         for weight in weights:
             max_weight,total_weight=max(weight,max_weight),total_weight+weight
         left,right=max_weight,total_weight
+        #print(left,right)
         while left<right:
             mid=(left+right)//2
+            #print(left,right,mid)
             total_day,cur_weight=1,0
             for weight in weights:
+                #print(cur_weight,left,right,mid,total_day)
                 if cur_weight+weight>mid:
                     total_day+=1
                     cur_weight=0
