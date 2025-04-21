046503c12b0493125550aa125d9abcd9b219dd2f
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-04-20 22:23:16 -0400

    Time: 19 ms (23.15%), Space: 18.1 MB (5.07%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        res,count=[],0
+        def backtrack(row,rowSet,colSet,diagonalSet,antiDiagonalSet,board):
+            if row==n:
+                res.append(board)
+            for col in range(n):
+                isValidCol = (row not in rowSet) and (col not in colSet) and (row+col not in diagonalSet) and (row-col not in antiDiagonalSet)
+                if isValidCol:
+                    backtrack(row+1,rowSet.union({row}),colSet.union({col}),diagonalSet.union({row+col}),antiDiagonalSet.union({row-col}),board+['.'*col+'Q'+'.'*(n-col-1)])
+        backtrack(0,set(),set(),set(),set(),[])
+        count=len(res)
+        return count
+
