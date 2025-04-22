7422ed7b289a15b480355dc51c3e278f92605e78
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-04-22 18:23:53 -0400

    Time: 267 ms (35.25%), Space: 20.3 MB (58.47%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,21 @@
 from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        res,visited,queue,rows,cols=0,set(),deque(),len(grid),len(grid[0])
         #bfs
-        def bfs():
-            while queue:
-                row,col=queue.popleft()
-                dirs=[[1,0],[-1,0],[0,1],[0,-1]]
-                for dr,dc in dirs:
-                    r,c=row+dr,col+dc
-                    if (r in range(rows) and c in range(cols) and grid[r][c]=='1' and (r,c) not in visited):
-                        queue.append((r,c))
-                        visited.add((r,c))
-
-
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j]=="1" and (i,j) not in visited:
-                    queue.append((i,j))
-                    visited.add((i,j))
-                    res+=1
-                    bfs()
+        res,m,n = 0,len(grid),len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    grid[i][j] = '0'
+                    self.bfs(grid,i,j)
+                    res += 1
         return res
+
+    def bfs(self,grid,i,j):
+        queue = deque([(i,j)])
+        while queue:
+            I,J = queue.popleft()
+            for i,j in [I+1,J],[I,J+1],[I-1,J],[I,J-1]:
+                if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == '1':
+                    grid[i][j] = '0'
+                    queue.append((i,j))
