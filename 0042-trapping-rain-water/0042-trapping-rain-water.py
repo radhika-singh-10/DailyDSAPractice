337a904d89c12022163d446bf84db07d313fdf71
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-04-22 18:37:34 -0400

    Time: 11 ms (74.8%), Space: 19.2 MB (93.39%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -3,7 +3,7 @@ class Solution:
         left,right,max_right,max_left,res=0,len(height)-1,0,0,0
         #min of (left,right) * (right-left+1)
         while left<right:
-            print(left,right,res,max_left,max_right)
+            #print(left,right,res,max_left,max_right)
             if height[left]<height[right]:
                 max_left=max(max_left,height[left])
                 res+=max_left-height[left]
