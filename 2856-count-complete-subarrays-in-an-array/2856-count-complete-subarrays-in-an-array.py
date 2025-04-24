ade5b7512f99d54cf6508a4eb0d5db3813d82d8f
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-04-24 11:35:34 -0400

    Time: 19 ms (43.01%), Space: 18 MB (46.5%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
@@ -3,7 +3,7 @@ class Solution:
         distinct_count,n,res,temp_count,r=len(set(nums)),len(nums),0,{},0
         #sliding windows
         for l in range(n):
-            #print(temp_count,l,r)
+            #print(temp_count,l,r,res)
             if l>0:
                 temp_count[nums[l-1]]-=1
                 if temp_count[nums[l-1]]==0:
@@ -11,10 +11,10 @@ class Solution:
             while r<n and len(temp_count)<distinct_count:
                 temp_count[nums[r]]=temp_count.get(nums[r],0)+1
                 r=r+1
-                #print(temp_count,l,r)
+                #print(temp_count,l,r,res)
             if len(temp_count)==distinct_count:
                 res+=n-r+1
                 
-                #print(temp_count,l,r)
+                #print(temp_count,l,r,res)
         return res
             
