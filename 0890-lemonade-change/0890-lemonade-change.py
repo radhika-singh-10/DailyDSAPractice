a25eceb1139860bc3b19cf66e015173449da99b1
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-05-03 12:58:52 -0400

    Time: 4 ms (50.97%), Space: 21.9 MB (34.18%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.py
+++ 0890-lemonade-change/0890-lemonade-change.py
@@ -1,24 +1,25 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
         n=len(bills)
-        changeMap={5:0,10:0,20:0}
+        fiveChange,tenChange=0,0
+        # changeMap={5:0,10:0,20:0}
         for i in range(n):
             if bills[i]==5:
-                changeMap[bills[i]]+=1
+                fiveChange+=1
             elif bills[i]==10:
-                if changeMap[5]>0:
-                    changeMap[5]-=1
-                    changeMap[bills[i]]+=1
+                if fiveChange>0:
+                    fiveChange-=1
+                    tenChange+=1
                 else:
                     return False
             else:
-                if changeMap[5]>0 and changeMap[10]>0:
-                    changeMap[5]-=1
-                    changeMap[10]-=1
-                    changeMap[bills[i]]+=1
-                elif changeMap[5]>=3:
-                    changeMap[5]-=3
-                    changeMap[bills[i]]+=1
+                if fiveChange>0 and tenChange>0:
+                    fiveChange-=1
+                    tenChange-=1
+                    # changeMap[bills[i]]+=1
+                elif fiveChange>=3:
+                    fiveChange-=3
+                    # changeMap[bills[i]]+=1
 
                 else:
                     return False
