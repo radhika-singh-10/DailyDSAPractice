1d7b458cc926b7dea29734598530eb4918dc3f0d
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-05-05 12:43:24 -0400

    Time: 101 ms (61.11%), Space: 28.5 MB (67.71%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,24 +1,12 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-
-
-        
-        #edge cases->height==1->height[0]
-        #any 0-> l=l+1
-        #only positive number? no negative or 0 or decimal
-        #what if - all the container height is same
-        if len(height)==1:
-            return height[0]
-        res,l,r,max_area=0,0,len(height)-1,0
+        n=len(height)
+        maxArea=0
+        l,r=0,n-1
         while l<r:
-            max_area=max(max_area,min(height[l],height[r])*abs(r-l))
-            if height[l]<height[r]:
-                l=l+1
-            #elif height[r]>=height[l]:
+            maxArea=max(maxArea,(r-l)*min(height[r],height[l]))
+            if height[r]>height[l]:
+                l+=1
             else:
-                r=r-1
-            # else:
-            #     l+=1
-            #     r-=1
-            
-        return max_area
+                r-=1
+        return maxArea
