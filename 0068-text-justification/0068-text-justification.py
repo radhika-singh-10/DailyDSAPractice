487b9aa3b70126768c354edc1c3b06a5ef6c8c49
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-05-28 13:21:59 -0400

    Time: 0 ms (100%), Space: 18.1 MB (20.12%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -12,27 +12,22 @@ class Solution:
                 i+=1
             return current_line
         def create_lines(line,i):
-            base_length = -1
+            line_length = -1
             for word in line:
-                base_length += len(word) + 1
-            extra_spaces = maxWidth - base_length
+                line_length += len(word) + 1
+            extra_spaces = maxWidth - line_length
             if len(line) == 1 or i == len(words):
                 return " ".join(line) + " " * extra_spaces
             word_count = len(line) - 1
             spaces_per_word = extra_spaces // word_count
             needs_extra_space = extra_spaces % word_count
-
             for j in range(needs_extra_space):
                 line[j] += " "
-
             for j in range(word_count):
                 line[j] += " " * spaces_per_word
-
             return " ".join(line)
-
         i = 0
         res=[]
-
         while i < len(words):
             current_line = get_words(i)
             i += len(current_line)
