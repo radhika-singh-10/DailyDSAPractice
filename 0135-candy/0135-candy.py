be1fe032d0224d9d284ad1f2949d838f4bc70171
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-06-02 11:56:15 -0400

    Time: 17 ms (49.28%), Space: 19.9 MB (36.16%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,17 +1,13 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         n,res=len(ratings),0
-        candies_left,candies_right=[1]*n,[1]*n
+        candies = [1]*n
         for i in range(1,n):
-            if ratings[i-1]<ratings[i]:
-                candies_left[i]=candies_left[i-1]+1
-
-        for i in range(n-2,-1,-1):
-            if ratings[i+1]<ratings[i]:
-                candies_right[i]=candies_right[i+1]+1
-
-        for i in range(n):
-            res+=max(candies_left[i],candies_right[i])
-        
-        return res
-        
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+        sum = candies[-1]
+        for i in range( n-2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+            sum += candies[i]
+        return sum
