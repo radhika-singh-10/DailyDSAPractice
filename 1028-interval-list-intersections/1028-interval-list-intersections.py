6d98d4a515314cc5f813cbe47a8fc6227c91972b
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-06-20 20:47:50 -0400

    Time: 7 ms (50.4%), Space: 18.8 MB (24.25%) - LeetHub

--- 1028-interval-list-intersections/1028-interval-list-intersections.py
+++ 1028-interval-list-intersections/1028-interval-list-intersections.py
@@ -5,9 +5,9 @@ class Solution:
             if firstList[i][0] <= secondList[j][1] and secondList[j][0] <= firstList[i][1]:
                 res.append([max(firstList[i][0], secondList[j][0]), min(firstList[i][1], secondList[j][1])])
             if firstList[i][1] <= secondList[j][1]:
-                i += 1
+                i+=1
             else:
-                j += 1
+                j+=1
 
         return res
                     
