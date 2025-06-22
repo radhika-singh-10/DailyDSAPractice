51bb7cf5afb2e8fc9ca3bc9c2f2d5231a60a62eb
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-06-22 11:01:07 -0400

    Time: 1 ms (13.16%), Space: 17.6 MB (96.32%) - LeetHub

--- 0953-reverse-only-letters/0953-reverse-only-letters.py
+++ 0953-reverse-only-letters/0953-reverse-only-letters.py
@@ -2,15 +2,26 @@ class Solution:
     def reverseOnlyLetters(self, s: str) -> str:
         n=len(s)
         pos,res,j=dict(),[],n-1
-        for i in range(n):
-            if s[i].isalpha():
-                while not s[j].isalpha():
-                    j-=1
-                res.append(s[j])
-                j-=1
+        letters = [c for c in s if c.isalpha()]
+        ans = []
+        for c in s:
+            if c.isalpha():
+                ans.append(letters.pop())
             else:
-                res.append(s[i])
-        return "".join(res)
+                ans.append(c)
+        return "".join(ans)
+        # #intuition- check if its alpha, and append normally if its not alphabet from backwards, else 
+        # for i in range(n):
+        #     if s[i].isalpha():
+        #         print(res,s[i])
+        #         while not s[j].isalpha():
+        #             j-=1
+        #         res.append(s[j])
+        #         j-=1
+                
+        #     else:
+        #         res.append(s[i])
+        # return "".join(res)
 
 
 
