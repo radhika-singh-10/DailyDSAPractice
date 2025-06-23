8125acf333d232e5f0d048dd018dc7ea312374c8
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-06-23 16:52:04 -0400

    Time: 11 ms (50%), Space: 30.4 MB (20.83%) - LeetHub

--- 1847-largest-subarray-length-k/1847-largest-subarray-length-k.py
+++ 1847-largest-subarray-length-k/1847-largest-subarray-length-k.py
@@ -2,7 +2,7 @@ class Solution:
     def largestSubarray(self, nums: List[int], k: int) -> List[int]:
         n,tempArr=len(nums),[]
         maxIndex=0
-        for i in range(1,n-(k-1)):
+        for i in range(1,n-k+1):
             if nums[i]>nums[maxIndex]:
                 maxIndex=i
         return nums[maxIndex:maxIndex+k]
