6c535cb75247266e88eeb19483a40d87bd80a054
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-06-28 13:29:18 -0400

    Time: 8 ms (61.28%), Space: 19.9 MB (42.23%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -6,10 +6,13 @@ class Solution:
         for num in nums:
             points[num]+=num
             max_num=max(max_num,num)
+            #print(points,max_num)
         max_points = [0] * (max_num+1)
         max_points[1] = points[1]
+        #print(max_points)
         for num in range(2,len(max_points)):
             max_points[num]=max(max_points[num-1],max_points[num-2]+points[num])
+            #print(max_points)
         return max_points[max_num]
         
 
