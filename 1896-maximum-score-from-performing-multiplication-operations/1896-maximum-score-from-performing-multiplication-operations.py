ac643e822f6b583c9d90ae7a7957ad0aef10d449
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-06-28 19:22:03 -0400

    Time: 219 ms (79.4%), Space: 26.6 MB (68.71%) - LeetHub

---
+++ 1896-maximum-score-from-performing-multiplication-operations/1896-maximum-score-from-performing-multiplication-operations.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
+        n,m=len(nums),len(multipliers)
+        dp=[[0]*(m+1) for _ in range(m+1)]
+        for i in range(m-1,-1,-1):
+            for l in range(i,-1,-1):
+                mul=multipliers[i]
+                r=n-1-(i-l)
+                dp[i][l]=max(mul*nums[l]+dp[i+1][l+1],mul*nums[r]+dp[i+1][l])
+        return dp[0][0]
