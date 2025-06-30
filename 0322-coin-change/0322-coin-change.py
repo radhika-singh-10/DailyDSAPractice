45917b542a47de2e5c78c20372342739c1bbcd22
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-06-30 12:26:11 -0400

    Time: 958 ms (28.91%), Space: 37.9 MB (15.64%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,11 +1,11 @@
 from functools import lru_cache
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # n=len(coins)
-        # if amount==0 or n==0:
-        #     return 0
-        # if min(coins)>amount:
-        #     return -1
+        n=len(coins)
+        if amount==0 or n==0:
+            return 0
+        if min(coins)>amount:
+            return -1
         #dp=[[0]*(n+1) for i in range(amount)]
         @lru_cache(None)
         def dfs(rem):
