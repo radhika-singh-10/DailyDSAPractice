aa5b02018c9007c8e5caeacfd769bae68b7bc2c7
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-07-08 13:13:01 -0400

    Time: 0 ms (100%), Space: 17.7 MB (87.11%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generate(self, n: int) -> List[List[int]]:
+        if n==0:
+            return []
+        elif n==1:
+            return [[1]]
+        
+        prevRows=self.generate(n-1)
+        prevRow=prevRows[-1]
+        curRow=[1]
+        for i in range(1,n-1):
+            curRow.append(prevRow[i-1]+prevRow[i])
+        curRow.append(1)
+        prevRows.append(curRow)
+        return prevRows
