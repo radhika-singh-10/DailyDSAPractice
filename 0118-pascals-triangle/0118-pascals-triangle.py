98774493379d44e9af9db50f2a1a76c4297538b6
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-07-08 13:22:31 -0400

    Time: 0 ms (100%), Space: 18 MB (16.34%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,18 +1,29 @@
 class Solution:
     def generate(self, n: int) -> List[List[int]]:
-        if n==0:
-            return []
-        elif n==1:
-            return [[1]]
+        triangle = []
+
+        for i in range(n):
+            row = [None for _ in range(i + 1)]
+            row[0], row[-1] = 1, 1
+            for j in range(1, len(row) - 1):
+                row[j] = triangle[i - 1][j - 1] + triangle[i- 1][j]
+            triangle.append(row)
+        return triangle
+
+
+        # if n==0:
+        #     return []
+        # elif n==1:
+        #     return [[1]]
         
-        prevRows=self.generate(n-1)
-        prevRow=prevRows[-1]
-        curRow=[1]
+        # prevRows=self.generate(n-1)
+        # prevRow=prevRows[-1]
+        # curRow=[1]
         
-        for i in range(1,n-1):
-            curRow.append(prevRow[i-1]+prevRow[i])
-            #print(curRow,prevRow[i-1]+prevRow[i])
+        # for i in range(1,n-1):
+        #     curRow.append(prevRow[i-1]+prevRow[i])
+        #     #print(curRow,prevRow[i-1]+prevRow[i])
         
-        curRow.append(1)
-        prevRows.append(curRow)
-        return prevRows
+        # curRow.append(1)
+        # prevRows.append(curRow)
+        # return prevRows
