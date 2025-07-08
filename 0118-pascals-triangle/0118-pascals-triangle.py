3d018b942c6733335796f9169feb7132729a39bd
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-07-08 13:16:18 -0400

    Time: 0 ms (100%), Space: 17.8 MB (58.89%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -8,8 +8,10 @@ class Solution:
         prevRows=self.generate(n-1)
         prevRow=prevRows[-1]
         curRow=[1]
+        
         for i in range(1,n-1):
             curRow.append(prevRow[i-1]+prevRow[i])
+        
         curRow.append(1)
         prevRows.append(curRow)
         return prevRows
