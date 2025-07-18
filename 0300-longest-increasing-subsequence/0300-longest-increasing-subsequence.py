6ae4a3a5d3d78f6cfe7edddc3f8a3b1ac47a7b00
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-07-17 22:00:56 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -2,27 +2,35 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         #dp
         n=len(nums)
-        dp=[0]*(n+1)
-        if n==1:
-            return 1
-        elif n==2:
-            if nums[0]<nums[1]:
-                return 2
-            else:
-                return 1
-        else:
-            dp=[nums[0]]
-            for i in range(n):
-                num=nums[i]
-                if num>dp[-1]:
-                    dp.append(num)
-                else:
-                    index=bisect_left(dp, num)
-                    if index==len(dp):
-                        dp.append(num)
-                    else:
-                        dp[index]=num
-            return len(dp)
+        dp=[1]*(n)
+        for i in range(1,n):
+            for j in range(i):
+                if nums[i]>nums[j]:
+                    dp[i]=max(dp[i],dp[j]+1)
+                
+        return dp[-1]
+
+        # dp=[0]*(n+1)
+        # if n==1:
+        #     return 1
+        # elif n==2:
+        #     if nums[0]<nums[1]:
+        #         return 2
+        #     else:
+        #         return 1
+        # else:
+        #     dp=[nums[0]]
+        #     for i in range(n):
+        #         num=nums[i]
+        #         if num>dp[-1]:
+        #             dp.append(num)
+        #         else:
+        #             index=bisect_left(dp, num)
+        #             if index==len(dp):
+        #                 dp.append(num)
+        #             else:
+        #                 dp[index]=num
+        #     return len(dp)
 
 
 
