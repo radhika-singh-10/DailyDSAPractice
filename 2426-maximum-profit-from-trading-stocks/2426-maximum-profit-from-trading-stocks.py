a63e631d5da8ffca3d32ed853bd7e57615f2d86f
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-07-21 17:31:57 -0400

    Time: 949 ms (63.6%), Space: 17.8 MB (91.17%) - LeetHub

--- 2426-maximum-profit-from-trading-stocks/2426-maximum-profit-from-trading-stocks.py
+++ 2426-maximum-profit-from-trading-stocks/2426-maximum-profit-from-trading-stocks.py
@@ -1,5 +1,22 @@
 class Solution:
     def maximumProfit(self, present: List[int], future: List[int], budget: int) -> int:
+        # n = len(present)
+        #top down approach - tle
+        # def dfs(i, rem):
+        #     if rem < 0:
+        #         return float('-inf')
+        #     if i >= n:
+        #         return 0
+        #     if (i, rem) in memo:
+        #         return memo[(i, rem)]
+        #     skip = dfs(i + 1, rem)
+        #     profit = future[i] - present[i]
+        #     new_rem = rem - present[i]
+        #     pick = profit + dfs(i + 1, new_rem)
+        #     memo[(i, rem)] = max(skip, pick)
+        #     return memo[(i, rem)]
+        # memo = dict()
+        # return dfs(0, budget)
         n=len(present)
         dp = [0 for _ in range(budget + 1)]
         for f,p in zip(future,present):
