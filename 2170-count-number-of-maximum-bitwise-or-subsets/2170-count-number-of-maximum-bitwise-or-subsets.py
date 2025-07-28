310f35d194e8995731393fb26de71473da1a02d9
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-07-28 16:03:32 -0400

    Time: 15 ms (94.65%), Space: 18.6 MB (19.55%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.py
@@ -7,18 +7,36 @@ class Solution:
         for num in nums:
             maxOr|=num
 
-        def backtrack(idx,curOr):
-            nonlocal res
-            if idx==len(nums) :
-                if curOr==maxOr:
-                    res+=1
-                return 
-            #include current number
-            backtrack(idx+1,curOr|nums[idx])
-            #skip current number
-            backtrack(idx+1,curOr)
-        backtrack(0,0)
-        return res
+        #memoization
+        memo=dict()#[[-1]*(maxoR+1) for _ in range(n)]
+
+        def memoBacktrack(idx, curOr):
+            if idx == n:
+                return 1 if curOr == maxOr else 0
+
+            if (idx, curOr) in memo:
+                return memo[(idx, curOr)]
+
+            without = memoBacktrack(idx + 1, curOr)
+            include = memoBacktrack(idx + 1, curOr | nums[idx])
+
+            memo[(idx, curOr)] = without + include
+            return memo[(idx, curOr)]
+
+        return memoBacktrack(0, 0)
+        #backtracking
+        # def backtrack(idx,curOr):
+        #     nonlocal res
+        #     if idx==len(nums) :
+        #         if curOr==maxOr:
+        #             res+=1
+        #         return 
+        #     #include current number
+        #     backtrack(idx+1,curOr|nums[idx])
+        #     #skip current number
+        #     backtrack(idx+1,curOr)
+        # backtrack(0,0)
+        # return res
 
 
         
