deb6c830526ce1f152b69c9bb682da4e7b0b528a
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-07-29 12:36:44 -0400

    Time: 0 ms (100%), Space: 17.7 MB (94.01%) - LeetHub

---
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -0,0 +1,73 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        freq,n=Counter(s),len(s)
+        maxCount,letter=0,''
+        for chr,count in freq.items():
+            if count>maxCount:
+                maxCount=count
+                letter=chr
+        if maxCount>(n+1)//2:
+            return ""
+        res=['']*len(s)
+        idx=0
+        while freq[letter]!=0:
+            res[idx]=letter
+            idx+=2
+            freq[letter]-=1
+        for char,count in freq.items():
+            while count>0:
+                if idx>=n:
+                    idx=1
+                res[idx]=char
+                idx+=2
+                count-=1
+        return ''.join(res)
+
+        # freq,n=Counter(s),len(s)
+        # res=[0]*n
+        # sorted(freq,reverse=True)
+        # for i in freq:
+        #     j=0
+        #     while j<n and freq[i]>0:
+        #         res[j]=i
+        #         j+=2
+        #         freq[i]-=1
+        # for i in range(len(res)-1):
+        #     if res[i]==res[i+1]:
+        #         return ""
+        
+        # return ""
+
+
+
+        # freq=Counter(s)
+        # res,l="",0
+        # sorted(freq.items(),reverse=True)
+        # print(freq)
+        # for i in freq:
+        #     l+=freq[i]
+        
+        # for i in freq:
+        #     temp_len=freq[i]
+        #     while temp_len>0:
+        #         res+=i+" "
+        #         temp_len-=1
+            
+        #     # if abs(freq[i]-freq[i+1])!=1:
+        #     #     return ""
+        #     # else:
+        #     #     res+=i
+        #     #     res+=i+1
+        #     #     freq[i]-=1
+        #     #     freq[i+1]-=1
+        # return "".join(res)
+        # # for i in range(len(s)-1):
+        # #     for j in range(i+1,len(s)):
+        # #         if s[i]==s[j] and s[j]==s[j+1]:
+        # #             return ""
+        # #         elif s[i]==s[j] and s[j]!=s[j+1]:
+        # #             t=s[j]
+        # #             s[j]=s[j+1]
+        # #             s[j+1]=t
+                
+        # # return s
