c034fe230b38b3221553d62fd93e87b257b60c18
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-08-05 12:14:36 -0400

    Time: 102 ms (95.78%), Space: 23.5 MB (76.92%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        baskets,n,l={},len(fruits),0
+        #sliding window - get max continuous len without breaking
+
+        for r,fruit in enumerate(fruits):
+            baskets[fruit]=baskets.get(fruit,0)+1
+            if len(baskets)>2:
+                baskets[fruits[l]]-=1
+                if baskets[fruits[l]]==0:
+                    del baskets[fruits[l]]
+                l+=1
+        return r-l+1
+
+
