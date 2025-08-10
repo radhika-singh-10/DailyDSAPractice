ad80c1619911d0fd1a6b1b2446abc64db133a897
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-08-10 17:58:48 -0400

    Time: 7 ms (30.65%), Space: 17.8 MB (52.87%) - LeetHub

---
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        count = collections.Counter(str(n))
+        print(count)
+        return any(count==Counter(str(1<<i)) for i in range(31))
+
