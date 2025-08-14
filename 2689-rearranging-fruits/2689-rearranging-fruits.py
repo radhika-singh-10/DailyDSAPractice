60b05cd6ae5b19b69aa52c4956a948bbc66804b8
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-08-14 17:01:57 -0400

    Time: 95 ms (69.29%), Space: 48.3 MB (28.5%) - LeetHub

--- 2689-rearranging-fruits/2689-rearranging-fruits.py
+++ 2689-rearranging-fruits/2689-rearranging-fruits.py
@@ -6,16 +6,16 @@ class Solution:
             if count % 2 != 0:
                 return -1
         fruits_to_swap,c1 = [], Counter(basket1)
-        for fruit, total_count in total.items():
-            target = total_count // 2
+        for fruit, count in total.items():
+            target = count // 2
             diff = c1.get(fruit, 0) - target
             for _ in range(abs(diff)):
                 fruits_to_swap.append(fruit)
 
         fruits_to_swap.sort()
-        min_val,total_cost = min(total.keys()), 0
+        min_val,cost = min(total.keys()), 0
         swaps_to_make = len(fruits_to_swap) // 2
         for i in range(swaps_to_make):
-            total_cost += min(fruits_to_swap[i], 2 * min_val)
+            cost += min(fruits_to_swap[i], 2 * min_val)
             
-        return total_cost
+        return cost
