291e6d9fc8a19a0c3e85fc13e301e0e137b90235
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-08-16 12:51:31 -0400

    Time: 91 ms (60.79%), Space: 21.1 MB (95.55%) - LeetHub

---
+++ 0305-number-of-islands-ii/0305-number-of-islands-ii.py
@@ -0,0 +1,62 @@
+class UnionFind:
+    def __init__(self,n):
+        self.parent=[-1]*n
+        self.rank=[0]*n
+        self.count=0
+    
+    def find(self,x):
+        if x!=self.parent[x]:
+            self.parent[x]=self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self,x,y):
+        rootx,rooty=self.find(x),self.find(y)
+        if rootx!=rooty:
+            if self.rank[rootx]<self.rank[rooty]:
+                self.parent[rootx]=rooty
+            elif self.rank[rooty]<self.rank[rootx]:
+                self.parent[rooty]=rootx
+            else:
+                self.parent[rootx]=rooty
+                self.rank[rooty]+=1
+            self.count-=1
+    
+    def add(self,x):
+        if self.parent[x]==-1:
+            self.parent[x]=x
+            self.count+=1
+    
+    def getCount(self):
+        return self.count
+
+class Solution:
+    def numIslands2(self, m: int, n: int, positions: List[List[int]]) -> List[int]:
+        #union-find/disjoint set-> find- parent component, union-count the related components
+        #it can be solved in klog(mn) -> if m*n -> converted r*n+c linear relation 
+        res=[]
+        neighbors=[[0, 1], [0, -1], [1, 0], [-1, 0]]
+        visited=set()
+
+        uf = UnionFind(m*n)
+        def isValid(r, c):
+            return 0 <= r < m and 0 <= c < n and uf.parent[r*n+c] != -1
+
+        for r,c in positions:
+            i=r*n+c
+            uf.add(i)
+            for dx,dy in neighbors:
+                nr,nc=r+dx,c+dy
+                if isValid(nr,nc):
+                    uf.union(i,nr*n+nc)
+            res.append(uf.getCount())
+        return res
+        
+
+
+
+
+
+
+
+        
+
