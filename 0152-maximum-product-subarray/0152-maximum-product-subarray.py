b1c0a14ea8325235b64ca46cd1737fe33e56738a
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-08-17 17:38:08 -0400

    Time: 7 ms (89.7%), Space: 18.5 MB (14.73%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -2,16 +2,26 @@ class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         if len(nums)==1:
             return nums[0]
-        max_prod = nums[0]  
-        cur_max, cur_min = nums[0], nums[0]  
-        for i in range(1, len(nums)):
-            if nums[i] < 0:  
-                cur_max, cur_min = cur_min, cur_max  
-            cur_max = max(nums[i], cur_max * nums[i])  
-            cur_min = min(nums[i], cur_min * nums[i]) 
+        res,prefMul,sufMul=0,1,1
+        for i in range(len(nums)):
+            if prefMul==0:
+                prefMul=1
+            if sufMul==0:
+                sufMul=1
+            prefMul*=nums[i]
+            sufMul*=nums[len(nums)-i-1]
+            res=max(res,max(prefMul,sufMul))
+        return res
+        # max_prod = nums[0]  
+        # cur_max, cur_min = nums[0], nums[0]  
+        # for i in range(1, len(nums)):
+        #     if nums[i] < 0:  
+        #         cur_max, cur_min = cur_min, cur_max  
+        #     cur_max = max(nums[i], cur_max * nums[i])  
+        #     cur_min = min(nums[i], cur_min * nums[i]) 
 
-            max_prod = max(max_prod, cur_max)  
-        return max_prod
+        #     max_prod = max(max_prod, cur_max)  
+        # return max_prod
         # max_prod,cur_prod=-float("inf"),1
         # #2 ptr apporach - l,rn=0,len(nums)
         # #max_prod=max(max_prod,cur_prod)
