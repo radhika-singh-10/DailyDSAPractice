8a8a680bd10a1fe858293bddd492e537090b5c45
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-08-26 11:10:28 -0400

    Time: 11 ms (63.51%), Space: 19.8 MB (41.88%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -4,34 +4,35 @@ class Solution:
             return []
         n, m, res = len(mat), len(mat[0]), []
 
-        valueStorage=collections.defaultdict(list)
-        for i in range(n):
-            for j in range(m):
-                valueStorage[(i+j)].append(mat[i][j])
-        for key,val in valueStorage.items():
+        # brute force
+        # valueStorage=collections.defaultdict(list)
+        # for i in range(n):
+        #     for j in range(m):
+        #         valueStorage[(i+j)].append(mat[i][j])
+        # for key,val in valueStorage.items():
             
-            if key%2!=0:
-                for v in range(len(val)):
-                    res.append(val[v])
-            else:
-                for v in range(len(val)-1,-1,-1):
-                    res.append(val[v])
-        return res
-        
-        # for d in range(n + m - 1):  
-        #     if d % 2 == 0:  
-        #         r = min(d, n - 1)
-        #         c = d - r
-        #         while r >= 0 and c < m:
-        #             res.append(mat[r][c])
-        #             r -= 1
-        #             c += 1
-        #     else:  
-        #         c = min(d, m - 1)
-        #         r = d - c
-        #         while r < n and c >= 0:
-        #             res.append(mat[r][c])
-        #             r += 1
-        #             c -= 1
-        
+        #     if key%2!=0:
+        #         for v in range(len(val)):
+        #             res.append(val[v])
+        #     else:
+        #         for v in range(len(val)-1,-1,-1):
+        #             res.append(val[v])
         # return res
+        
+        for d in range(n + m - 1):  
+            if d % 2 == 0:  
+                r = min(d, n - 1)
+                c = d - r
+                while r >= 0 and c < m:
+                    res.append(mat[r][c])
+                    r -= 1
+                    c += 1
+            else:  
+                c = min(d, m - 1)
+                r = d - c
+                while r < n and c >= 0:
+                    res.append(mat[r][c])
+                    r += 1
+                    c -= 1
+        
+        return res
