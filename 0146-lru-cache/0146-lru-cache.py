c1efe8817c6cbed9d58edcc822028e1dd12ed934
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-12-18 21:34:01 -0800

    Time: 102 ms (90.4%), Space: 77.5 MB (99.17%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,35 +1,55 @@
 from collections import OrderedDict
 class LRUCache:
-    
+
     def __init__(self, capacity: int):
         self.capacity=capacity
-        # self.cache={} #to get the frequency stored as key-value
-        # self.order=deque()#to get the order 
         self.cache=OrderedDict()
 
-    def get(self, key: int) -> int:
-        #if key is present, we move the most recently accessed to the last
-        if key in self.cache:
-            # self.order.remove(key)
-            # self.order.append(key)
-            self.cache.move_to_end(key)
-            return self.cache[key]
-        return -1
 
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    
     def put(self, key: int, value: int) -> None:
-        # If key already exists, we update the value and move it to the end of the cache, then we check If capacity is exceeded, we remove the least recently used key
         if key in self.cache:
             self.cache.move_to_end(key)
-            #self.order.remove(key)
-        # elif len(self.cache) >= self.capacity:
-        #     lru_key = self.order.popleft()
-        #     del self.cache[lru_key]
-       
-        self.cache[key] = value
-        if len(self.cache)>self.capacity:
+        self.cache[key]=value
+        if self.capacity < len(self.cache):
             self.cache.popitem(False)
-        # self.order.append(key)
-        # print(self.cache,self.order)
+
+        
+    # def __init__(self, capacity: int):
+    #     self.capacity=capacity
+    #     # self.cache={} #to get the frequency stored as key-value
+    #     # self.order=deque()#to get the order 
+    #     self.cache=OrderedDict()
+
+    # def get(self, key: int) -> int:
+    #     #if key is present, we move the most recently accessed to the last
+    #     if key in self.cache:
+    #         # self.order.remove(key)
+    #         # self.order.append(key)
+    #         self.cache.move_to_end(key)
+    #         return self.cache[key]
+    #     return -1
+
+    # def put(self, key: int, value: int) -> None:
+    #     # If key already exists, we update the value and move it to the end of the cache, then we check If capacity is exceeded, we remove the least recently used key
+    #     if key in self.cache:
+    #         self.cache.move_to_end(key)
+    #         #self.order.remove(key)
+    #     # elif len(self.cache) >= self.capacity:
+    #     #     lru_key = self.order.popleft()
+    #     #     del self.cache[lru_key]
+       
+    #     self.cache[key] = value
+    #     if len(self.cache)>self.capacity:
+    #         self.cache.popitem(False)
+    #     # self.order.append(key)
+    #     # print(self.cache,self.order)
 
 
 
