7b1593f20ef4759013149d8cdd5a367ace7a5c41
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-12-25 12:32:28 -0800

    Time: 75 ms (79.27%), Space: 36.3 MB (76.59%) - LeetHub

---
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.py
@@ -0,0 +1,19 @@
+class Solution:
+    def goodDaysToRobBank(self, security: List[int], time: int) -> List[int]:
+        if time==0:
+            return [idx for idx in range(0,len(security))]
+        res=[]
+        n=len(security)
+        non_incr_left,non_decr_right=[0]*n,[0]*n
+        for i in range(1,n):
+            if security[i-1]>=security[i]:
+                non_incr_left[i]=non_incr_left[i-1]+1
+                continue
+        for i in range(n-2,-1,-1):
+            if security[i]<=security[i+1]:
+                non_decr_right[i]=non_decr_right[i+1]+1
+                continue
+        for i in range(time,n-time):
+            if non_incr_left[i]>=time and non_decr_right[i]>=time:
+                res.append(i)
+        return res
