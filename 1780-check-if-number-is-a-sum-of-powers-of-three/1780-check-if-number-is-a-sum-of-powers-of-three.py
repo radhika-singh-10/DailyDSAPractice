15a061cbf661f130bd7ee18c639f945c5977d2d0
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2026-01-01 11:04:01 -0800

    Time: 226 ms (7.81%), Space: 17.3 MB (89.69%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.py
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,13 +1,17 @@
 class Solution:
+    def isPowerOfThree(self,power,num):
+        if num==0 :
+            return True
+        if 3**power>num:
+            return False
+        addPower=self.isPowerOfThree(power+1,num-3**power)
+        skipPower=self.isPowerOfThree(power+1,num)
+        return addPower or skipPower
 
+        
     def checkPowersOfThree(self, n: int) -> bool:
         remainder=[]
         if n==1:
             return True
-        while n>1:
-            if n%3==2:
-                return False
-            n=n//3
-        return True
-
+        return self.isPowerOfThree(0,n)
         
