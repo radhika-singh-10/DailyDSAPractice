1f4f3ac698d45911b16b088ccaa918ea61328954
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2026-01-01 20:15:13 -0800

    Time: 4 ms (33.91%), Space: 17.5 MB (93.74%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,29 +1,59 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         #binary search
-        if len(nums1) > len(nums2):
+        n,m=len(nums1),len(nums2)
+        if n > m:
             return self.findMedianSortedArrays(nums2, nums1)
-        m, n = len(nums1), len(nums2)
-        left, right = 0, m
-        while left <= right:
-            midA = (left + right) // 2
-            midB = (m + n + 1) // 2 - midA
-            leftA = (float("-inf") if midA == 0 else nums1[midA - 1])
-            leftB = (float("-inf") if midB == 0 else nums2[midB - 1])
-            rightA = float("inf") if midA == m else nums1[midA]
-            rightB = float("inf") if midB == n else nums2[midB]
 
-            if leftA <= rightB and leftB <= rightA:
-                if (m + n) % 2 == 0:
-                    return (
-                        max(leftA, leftB) + min(rightA, rightB)
-                    ) / 2
+        low, high = 0, n
+        while low <= high:
+            m1 = (low + high) // 2
+            m2 = (n + m + 1) // 2 - m1
+
+            l1 = nums1[m1 - 1] if m1-1 >= 0 else float("-inf")
+            r1 = nums1[m1]     if m1 < n else float("inf")
+
+            l2 = nums2[m2 - 1] if m2-1 >= 0 else float("-inf")
+            r2 = nums2[m2]     if m2 < m else float("inf")
+
+            if l1 <= r2 and l2 <= r1:
+                if (n + m) % 2 == 0:
+                    return (max(l1, l2) + min(r1, r2)) / 2.0
                 else:
-                    return max(leftA, leftB)
-            elif leftA > rightB:
-                right = midA - 1
-            else:
-                left = midA + 1
+                    return float(max(l1, l2))
+            elif l1 > r2:
+                high = m1 - 1
+            else:  # l2 > r1
+                low = m1 + 1
+
+        return 0.0
+
+
+
+
+        # if len(nums1) > len(nums2):
+        #     return self.findMedianSortedArrays(nums2, nums1)
+        # m, n = len(nums1), len(nums2)
+        # left, right = 0, m
+        # while left <= right:
+        #     midA = (left + right) // 2
+        #     midB = (m + n + 1) // 2 - midA
+        #     leftA = (float("-inf") if midA == 0 else nums1[midA - 1])
+        #     leftB = (float("-inf") if midB == 0 else nums2[midB - 1])
+        #     rightA = float("inf") if midA == m else nums1[midA]
+        #     rightB = float("inf") if midB == n else nums2[midB]
+
+        #     if leftA <= rightB and leftB <= rightA:
+        #         if (m + n) % 2 == 0:
+        #             return (
+        #                 max(leftA, leftB) + min(rightA, rightB)
+        #             ) / 2
+        #         else:
+        #             return max(leftA, leftB)
+        #     elif leftA > rightB:
+        #         right = midA - 1
+        #     else:
+        #         left = midA + 1
 
         #combine the two arrays 
         #total length - l1+l2
