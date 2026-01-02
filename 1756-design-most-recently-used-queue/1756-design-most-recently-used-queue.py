9fe6e0f0f58d5a0ef1fe3d9e36eb8f6e2405c1ad
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2026-01-01 20:30:55 -0800

    Time: 22 ms (85.22%), Space: 18.4 MB (98.26%) - LeetHub

---
+++ 1756-design-most-recently-used-queue/1756-design-most-recently-used-queue.py
@@ -0,0 +1,12 @@
+class MRUQueue:
+
+    def __init__(self, n: int):
+        self.queue=[i for i in range(1,n+1)]
+
+    def fetch(self, k: int) -> int:
+        value=self.queue.pop(k-1)
+        self.queue.append(value)
+        return value
+# Your MRUQueue object will be instantiated and called as such:
+# obj = MRUQueue(n)
+# param_1 = obj.fetch(k)
