527d9ea740d42b6944ceb51ef4e283e1ad915de0
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2026-01-03 01:13:04 -0800

    Time: 15 ms (100%), Space: 17.5 MB (99.74%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
@@ -2,10 +2,7 @@ class Solution:
     def minimumLength(self, s: str) -> int:
         n=len(s)
         l,r=0,len(s)-1
-        prefix,suffix=[],[]
         while l<r and s[l]==s[r]:
-            # prefix.append(s[l])
-            # suffix.append(s[r])
             ch=s[l]
             while l<=r and ch==s[l]:
                 l+=1
