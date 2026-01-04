141020fc9843c15a9f0c8c7a434d23b1811f3677
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2026-01-03 20:30:13 -0800

    Time: 166 ms (63.58%), Space: 52.6 MB (81.79%) - LeetHub

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.py
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.py
@@ -1,12 +1,11 @@
 class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
         meetings.sort()
-        free_days = 0
-        prev_end = 0
+        res,prev_end = 0,0
         for start, end in meetings:
             if start > prev_end + 1:
-                free_days += start - prev_end - 1
+                res += start - prev_end - 1
             prev_end = max(prev_end, end)
-        free_days += days - prev_end
-        return free_days
+        res += days - prev_end
+        return res
 
