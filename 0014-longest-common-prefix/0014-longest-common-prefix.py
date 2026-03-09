3a8e806f8b62237415a35aaafdfc0285329699cf
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2026-03-09 09:35:15 -0700

    Time: 0 ms (100%), Space: 19.3 MB (69.96%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,14 +1,32 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        n=len(strs)
-        if n==0:
+        if not strs:
             return ""
-        prefix=strs[0]
-        for i in range(1,n):
-            while strs[i].find(prefix)!=0:
-                prefix=prefix[0:len(prefix)-1]
-                if prefix=="":
-                    return ""
+        def lcp(leftStr,rightStr):
+            minLen=min(len(leftStr),len(rightStr))
+            for i in range(minLen):
+                if leftStr[i]!=rightStr[i]:
+                    return leftStr[:i]
+            return leftStr[:minLen]
 
-        return prefix
+        def divideAndConquer(strs,l,r):
+            if l==r:
+                return strs[l]
+            else:
+                m=(l+r)//2
+                lcpLeft=divideAndConquer(strs,l,m)
+                lcpRight=divideAndConquer(strs,m+1,r)
+                return lcp(lcpLeft,lcpRight)
+        return divideAndConquer(strs,0,len(strs)-1)
+        # n=len(strs)
+        # if n==0:
+        #     return ""
+        # prefix=strs[0]
+        # for i in range(1,n):
+        #     while strs[i].find(prefix)!=0:
+        #         prefix=prefix[0:len(prefix)-1]
+        #         if prefix=="":
+        #             return ""
+
+        # return prefix
         
