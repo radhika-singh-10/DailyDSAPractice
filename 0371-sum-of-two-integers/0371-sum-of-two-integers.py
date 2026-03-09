2fb20d8d942d784bd4a00652b1bea87571782515
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2026-03-09 11:20:06 -0700

    Time: 0 ms (100%), Space: 19.2 MB (84.35%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        x, y = abs(a), abs(b)
+        if x<y:
+            return self.getSum(b,a)
+        sign = 1 if a>0 else -1
+        if a*b>=0:
+            while y:
+                ans=x^y
+                carry= (x&y)<<1
+                x,y=ans,carry
+        else:
+            while y:
+                ans=x^y
+                borrow=((~x)&y)<<1
+                x,y=ans,borrow
+        return x*sign
