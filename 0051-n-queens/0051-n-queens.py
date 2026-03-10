882e5256afd5426256ad4696122ca0696e3e95b9
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2026-03-09 20:01:29 -0700

    Time: 10 ms (73.78%), Space: 19.7 MB (88.15%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -4,16 +4,38 @@ class Solution:
         #we will apply backtracking
         # we will check col by col  not row by row
         res=[]
-        def backtrackCheck(row,rowSet,colSet,diagonalSet,antiDiagonalSet,board):
-            if row==n:
-                res.append(board)
-            for col in range(n):   
-                isValidRowCol=(row not in rowSet) and (col not in colSet) and (row+col not in diagonalSet) and (row-col not in antiDiagonalSet)
-                if isValidRowCol:
-                    backtrackCheck(row+1,rowSet.union({row}),colSet.union({col}),diagonalSet.union({row+col}),antiDiagonalSet.union({row-col}),board+['.'*col+'Q'+'.'*(n-col-1)])
-        backtrackCheck(0,set(),set(),set(),set(),[])
-        return res
+        def create_board(state):
+            board = []
+            for row in state:
+                board.append("".join(row))
+            return board
 
+        def backtrack(row, diagonals, anti_diagonals, cols, state):
+            if row == n:
+                ans.append(create_board(state))
+                return
 
+            for col in range(n):
+                curr_diagonal = row - col
+                curr_anti_diagonal = row + col
+                if (
+                    col in cols
+                    or curr_diagonal in diagonals
+                    or curr_anti_diagonal in anti_diagonals
+                ):
+                    continue
+                cols.add(col)
+                diagonals.add(curr_diagonal)
+                anti_diagonals.add(curr_anti_diagonal)
+                state[row][col] = "Q"
+                backtrack(row + 1, diagonals, anti_diagonals, cols, state)
+                cols.remove(col)
+                diagonals.remove(curr_diagonal)
+                anti_diagonals.remove(curr_anti_diagonal)
+                state[row][col] = "."
 
+        ans = []
+        empty_board = [["."] * n for _ in range(n)]
+        backtrack(0, set(), set(), set(), empty_board)
+        return ans
 
