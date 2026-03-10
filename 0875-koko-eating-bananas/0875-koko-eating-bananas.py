5ed11962f07ca8654daec1f8499222c06e4fad67
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2026-03-10 00:26:13 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -8,6 +8,7 @@ class Solution:
             hr_spent=0
             for pile in piles:
                 hr_spent+=math.ceil(pile/m)
+                print(pile,hr_spent)
             #if koko can finish eating in h, she can finish eating in h+1
             #else she cant finsih eating h-1 
             if hr_spent<=h:
