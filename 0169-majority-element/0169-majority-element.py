b4c4b02069958770fbec916d1017acd587f1bdb9
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2026-03-15 19:13:56 -0700

    Time: 4 ms (70.96%), Space: 21.1 MB (96.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -7,5 +7,6 @@ class Solution:
             if count==0:
                 candidate=num
             count+=1 if num ==candidate else -1
+            #print(candidate,num,count)
         return candidate
         
