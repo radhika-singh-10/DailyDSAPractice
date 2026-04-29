e143312195bce8b8e209752749d6d2e16c769e20
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2026-04-29 08:57:01 -0700

    Time: 0 ms (100%), Space: 19.1 MB (97.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -10,7 +10,7 @@ class Solution:
             dp[0],dp[1]=1,1
             res=0
             for i in range(2,n+1): 
-                dp[i]=dp[i-1]+dp[i-2]
+                dp[i]=max(dp[i],dp[i-1]+dp[i-2])
             return dp[n]
 
             #time limit exceeded
