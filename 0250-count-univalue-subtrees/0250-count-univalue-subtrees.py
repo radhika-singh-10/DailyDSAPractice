e516e84c76a6d338a6334d65cb753edde8b2dfbc
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2026-05-09 23:10:02 -0700

    Time: 0 ms (100%), Space: 19.5 MB (48.69%) - LeetHub

---
+++ 0250-count-univalue-subtrees/0250-count-univalue-subtrees.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    def dfs(self, node):
+        if not node:
+            return True
+        isLeftUnique = self.dfs(node.left)
+        isRightUnique = self.dfs(node.right)
+
+        if isLeftUnique and isRightUnique:
+            if node.left and node.val != node.left.val:
+                return False
+            if node.right and node.val != node.right.val:
+                return False
+            self.count += 1
+            return True
+        return False
+
+
+        
+            
+        
+    def countUnivalSubtrees(self, root: Optional[TreeNode]) -> int:
+        self.count=0
+        if not root:
+            return 0
+        self.dfs(root)
+        return self.count
+        
